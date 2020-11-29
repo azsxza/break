f45d760aa3937ee3bdda799cccb2f73b8a99af29
程凯 <aaz63113270@outlook.com>
2020-11-30 03:38:16 +0800

    update game level

---
+++ brickGame/FGameLevel.cpp
@@ -0,0 +1,18 @@
+#include "FGameLevel.h"
+
+void FGameLevel::Load(const string & FileName, GLuint LevelWidth, GLuint LevelHeight)
+{
+}
+
+void FGameLevel::Draw(FSprite & Sprite)
+{
+}
+
+GLboolean FGameLevel::IsCompleted()
+{
+	return GLboolean();
+}
+
+void FGameLevel::Init(vector<vector<GLuint>> TitleData, GLuint LevelWidth, GLuint LevelHeight)
+{
+}
---
+++ brickGame/FGameLevel.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <glad/glad.h>
+#include <string>
+#include "FGameObject.h"
+#include "FSprite.h"
+
+using namespace std;
+
+class FGameLevel
+{
+public:
+	vector<FGameObject> Bricks;
+	
+
+	FGameLevel() {}
+	void Load(const string& FileName, GLuint LevelWidth, GLuint LevelHeight);
+
+	void Draw(FSprite& Sprite);
+	GLboolean IsCompleted();
+private:
+	void Init(vector<vector<GLuint>> TitleData, GLuint LevelWidth, GLuint LevelHeight);
+
+};
